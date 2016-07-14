27b392d3380c14026a6a6aeb6cea49b64e2182c0
Zhongjie Shen <shenzj1994@gmail.com>
2016-07-14 10:42:53 -0400

    Added AdMob but failed to display anything.

--- app/src/main/java/com/zhongjieshen/mytestapp/MainActivity.java
+++ app/src/main/java/com/zhongjieshen/mytestapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.zhongjieshen.mytestapp;
 
+import android.content.Context;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(),"ca-app-pub-3485573207705717~1922889185");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+                .addTestDevice("26876D708590843C")  // An example device ID
+                .build();
+
+        mAdView.loadAd(adRequest);
     }
+
+
 }
